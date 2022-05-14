be3b0478a11a8f6ccc4e84d3e12cc8b92d4e23d2
LeeSeokJune <indians1720@gmail.com>
2022-05-14 22:12:45 +0900

    pet register

--- django/user/views.py
+++ django/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .serializers import PetSerializer, UserSerializer
 from .models import User,Pet
 # Create your views here.
 
@@ -35,12 +35,30 @@ def login(request):
         else :
             print('done')
             return Response(serializer.data)
+
+# 펫 model save
 @api_view(['POST'])
 def pet_register(request):
     if request.method == 'POST':
+        p_info = Pet.inputPet(request)
         try:
             pet_info = Pet.objects.get(
                 u_id = User.objects.get(u_id = request.POST['u_id']),
             )
         except:
+            p_info.save()
+            return Response('register')
         #여기를 어떻게 해야 잘했다고 소문이 날까
+
+@api_view(['POST'])
+def pet(request):
+    if request.method =='POST':
+        try : 
+            pet_info = Pet.objects.filter(
+                u_id = User.objects.get(u_id = request.POST['u_id'])
+            )
+        except :
+            return Response('no pet')
+        else: 
+            serializer = PetSerializer(pet_info, many = True)
+            return Response(serializer)
